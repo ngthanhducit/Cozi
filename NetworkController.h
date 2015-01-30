ff2b7fe0e8eda02fcfd38572c055e772b54bdcdd
ngthanhducit <ngthanhducit@hotmail.com>
2015-01-30 18:15:27 +0700

    add new function

--- NetworkController.h
+++ NetworkController.h
@@ -10,23 +10,39 @@
 #import "Cozi/DataWall.h"
 #import "Cozi/Helper.h"
 #import "Cozi/NetworkCommunication.h"
+#import "PNetworkCommunication.h"
 
-@interface NetworkController : NSObject
+@interface NetworkController : NSObject <PNetworkCommunication>
 {
     Helper          *helperIns;
     NetworkCommunication *networkIns;
+    
+    NSMutableArray      *instances;
 }
 
 + (id) shareInstance;
 
+- (void) addListener:(id)instance;
+- (void) removeListener:(id)instance;
+
 - (void) getUploadPostUrl;
 - (void) resultUploadImagePost:(int)code withCodeThumb:(int)codeThumb;
-- (void) addPost:(NSString*)_content withImage:(NSString*)_image withVideo:(NSString*)_video withLocation:(NSString*)_location withClientKey:(NSString*)clientKey;
+- (void) addPost:(NSString*)_content withImage:(NSString*)_image withImageThumb:(NSString*)_imageThumb withVideo:(NSString*)_video withLocation:(NSString*)_location withClientKey:(NSString*)clientKey withCode:(int)code;
+- (void) addMood:(NSString*)_content withClientKey:(NSString*)clientKey withCode:(int)code;
 - (void) getWall:(int)_countPost withClientKeyStart:(NSString*)_startClientKey;
 - (void) getWallByUser:(int)_countPost withClientKeyStart:(NSString*)_startClientKey withUserID:(int)_userID;
 - (void) getNoise:(int)_countPost withClientKeyStart:(NSString*)_startClientKey;
 
 //LIKE
 - (void) addPostLike:(DataWall*)_wall;
+- (void) removePostLike:(DataWall*)_wall;
 
+- (void) addComment:(NSString*)_content withImageName:(NSString*)_imgName withPostClientKey:(NSString*)_postClientKey withCommentClientKey:(NSString*)_commentClientKey withUserPostID:(int)_userPostComment;
+
+- (void) getUserProfile:(int)_userID;
+- (void) getUserFollower:(int)_userID;
+- (void) getUserFollowing:(int)_userID;
+- (void) getUserPost:(int)_userID withCountPost:(int)_countPost withClientKey:(NSString*)_clientKey;
+
+- (void) addFollow:(int)_userFollowID;
 @end
--- NetworkController.m
+++ NetworkController.m
@@ -29,7 +29,18 @@
     return self;
 }
 
+- (void) addListener:(id)instance{
+
+    [instances addObject:instance];
+    
+}
+
+- (void) removeListener:(id)instance{
+    [instances removeObject:instance];
+}
+
 - (void) initVariable{
+    instances = [NSMutableArray new];
     helperIns = [Helper shareInstance];
     networkIns = [NetworkCommunication shareInstance];
 }
@@ -44,8 +55,13 @@
     [networkIns sendData:cmd];
 }
 
-- (void) addPost:(NSString*)_content withImage:(NSString*)_image withVideo:(NSString*)_video withLocation:(NSString*)_location withClientKey:(NSString*)clientKey{
-    NSString *cmd = [NSString stringWithFormat:@"ADDPOST{%@}%@}%@}%@}%@<EOF>", _content, _image, _video, _location, clientKey];
+- (void) addPost:(NSString*)_content withImage:(NSString*)_image withImageThumb:(NSString*)_imageThumb withVideo:(NSString*)_video withLocation:(NSString*)_location withClientKey:(NSString*)clientKey withCode:(int)code{
+    NSString *cmd = [NSString stringWithFormat:@"ADDPOST{%@}%@}%@}%@}%@}%i<EOF>", _content, [NSString stringWithFormat:@"%@$%@", _imageThumb, _image], _video, _location, clientKey, code];
+    [networkIns sendData:cmd];
+}
+
+- (void) addMood:(NSString*)_content withClientKey:(NSString*)clientKey withCode:(int)code{
+    NSString *cmd = [NSString stringWithFormat:@"ADDPOST{%@}%@}%@}%@}%@}%i<EOF>", _content, @"", @"", @"", clientKey, code];
     [networkIns sendData:cmd];
 }
 
@@ -70,4 +86,65 @@
     [networkIns sendData:_cmdAddLike];
 }
 
+- (void) removePostLike:(DataWall*)_wall{
+    
+    NSString *_decodeClientKey = [helperIns encodedBase64:[_wall.clientKey dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString *_cmdAddLike = [NSString stringWithFormat:@"REMOVEPOSTLIKE{%@}%i<EOF>", _decodeClientKey, _wall.userPostID];
+    [networkIns sendData:_cmdAddLike];
+    
+}
+
+- (void) addComment:(NSString*)_content withImageName:(NSString*)_imgName withPostClientKey:(NSString*)_postClientKey withCommentClientKey:(NSString*)_commentClientKey withUserPostID:(int)_userPostComment{
+    
+    NSString *_encodeContent = [helperIns encodedBase64:[_content dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString *_encodeImageName = [helperIns encodedBase64:[_imgName dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString *_encodePostClientKey = [helperIns encodedBase64:[_postClientKey dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString *_encodeCommentClientKey = [helperIns encodedBase64:[_commentClientKey dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSString *cmd = [NSString stringWithFormat:@"ADDCOMMENT{%@}%@}%@}%@}%i<EOF>", _encodeContent, _encodeImageName, _encodePostClientKey, _encodeCommentClientKey, _userPostComment];
+    [networkIns sendData:cmd];
+}
+
+- (void) getUserProfile:(int)_userID{
+    NSString *cmd = [NSString stringWithFormat:@"GETUSERPROFILE{%i<EOF>", _userID];
+    [networkIns sendData:cmd];
+}
+
+- (void) getUserFollower:(int)_userID{
+    NSString *cmd = [NSString stringWithFormat:@"GETUSERFOLLOWER{%i<EOF>", _userID];
+    [networkIns sendData:cmd];
+}
+
+- (void) getUserFollowing:(int)_userID{
+    NSString *cmd = [NSString stringWithFormat:@"GETUSERFOLLOWING{%i<EOF>", _userID];
+    [networkIns sendData:cmd];
+}
+
+- (void) getUserPost:(int)_userID withCountPost:(int)_countPost withClientKey:(NSString*)_clientKey{
+    NSString *_encodeClientKey = [helperIns encodedBase64:[_clientKey dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString *cmd = [NSString stringWithFormat:@"GETUSERPOST{%i}%i}%@<EOF>", _userID, _countPost, _encodeClientKey];
+    [networkIns sendData:cmd];
+}
+
+- (void) addFollow:(int)_userFollowID{
+    NSString *cmd = [NSString stringWithFormat:@"USERADDFOLLOW{%i<EOF>", _userFollowID];
+    [networkIns sendData:cmd];
+}
+
+- (void) setResult:(NSString *)_strResult{
+    if (instances != nil && [instances count] > 0) {
+//        int count = (int)[instances count];
+        for (int i = 0; i < (int)[instances count]; i++) {
+            id ins = [instances objectAtIndex:i];
+            if ([ins conformsToProtocol:@protocol(PNetworkCommunication)]) {
+                
+                [ins setResult:_strResult];
+                
+//                [instances removeObjectAtIndex:i];
+                
+//                i--;
+            }
+        }
+    }
+}
 @end
