7d943dfe8f98012dd12f0281ac6bc2f08576a504
ngthanhducit <ngthanhducit@hotmail.com>
2015-02-13 23:05:26 +0700

    add new image

---
+++ PNetworkCommunication.h
@@ -0,0 +1,16 @@
+//
+//  PNetworkCommunication.h
+//  Cozi
+//
+//  Created by Nguyen Thanh Duc on 1/20/15.
+//  Copyright (c) 2015 ChjpCoj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol PNetworkCommunication <NSObject>
+
+- (void) setResult:(NSString*)_strResult;
+
+@end
